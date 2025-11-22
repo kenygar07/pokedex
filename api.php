744e9774e204beec2f1c780d82aa9b5a6edce115
kenygar07 <keny.garcia@catolica.edu.sv>
2025-11-22 01:14:43 -0600

    si funciona

--- api.php
+++ api.php
@@ -7,6 +7,8 @@ header('Access-Control-Allow-Headers: Content-Type');
 require_once __DIR__ . '/classes/PokemonAPI.php';
 
 // Handle preflight requests
+//probando si funciona
+//ayuda
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
